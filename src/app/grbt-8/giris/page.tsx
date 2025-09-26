f991a018ab73999055d6fa199f312e9ea0127238
GRBT8AP Backup <backup@grbt8ap.com>
2025-09-26 23:42:38 +0200

    Fix: Remove required attribute from PIN input to disable validation

--- src/app/grbt-8/giris/page.tsx
+++ src/app/grbt-8/giris/page.tsx
@@ -59,7 +59,6 @@ export default function AdminGiris() {
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="PIN"
               maxLength={6}
-              required
             />
           </div>
 
